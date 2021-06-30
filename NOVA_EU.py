699c15711093d366cc5f97a3a8cae1b9d35120fb
aslk76 <q_shishani@yahoo.com>
2021-06-30 12:58:09 +0300

    movef functios in main py

--- NOVA_EU.py
+++ NOVA_EU.py
@@ -18,10 +18,10 @@ import re
 from dotenv import load_dotenv
 import gspread_asyncio
 from google.oauth2.service_account import Credentials
+from string import ascii_lowercase
 
 
 from constants import *
-from functions import convert_si_to_number, search_nested_alliance, search_nested_horde, checkPers, get_embedded_fields
 
 def get_creds():
     creds = Credentials.from_service_account_file("/NOVA/AutoBot/novabot-256801-85f98aa21edc.json")
@@ -110,6 +110,7 @@ class rio_conf:
     base: str = "https://raider.io"
     role_threshhold: int = 1300
 
+# region Functions
 async def record_usage(ctx):
     async with ctx.bot.ops_pool.acquire() as conn:
         async with conn.cursor() as cursor:
@@ -126,6 +127,84 @@ async def record_usage(ctx):
             else:
                 val = (ctx.author.display_name, ctx.command.name, "no arguments passed", ctx.message.created_at.replace(microsecond=0))
             await cursor.execute(query, val)
+
+
+def convert_si_to_number(i):
+    if not i:
+        return 0
+
+    total_stars = 0
+    alpha = ascii_lowercase.replace("k", "").replace("m", "").replace("b", "")
+
+    i = i.strip().replace(",", ".").lower()
+
+    if not i or any(char in alpha for char in i):
+        return total_stars
+
+    if len(i) >= 1:
+        if 'k' in i:
+            total_stars = float(i.replace('k', '')) * 1000
+        elif 'm' in i:
+            total_stars = float(i.replace('m', '')) * 1000000
+        elif 'b' in i:
+            total_stars = float(i.replace('b', '')) * 1000000000
+        else:
+            total_stars = int(i)
+
+    return int(total_stars)
+
+
+async def search_nested_alliance(mylist, val):
+    for i in range(len(mylist)):
+        for j in range(len(mylist[i])):
+            # print i,j
+            if mylist[i][j] == val:
+                return mylist[i][0]
+    return None
+
+
+async def search_nested_horde(mylist, val):
+    for i in range(len(mylist)):
+        for j in range(len(mylist[i])):
+            # print i,j
+            if mylist[i][j] == val:
+                return mylist[i][1]
+    return None
+
+
+async def checkPers(id :int):
+    async with bot.mplus_pool.acquire() as conn:
+        async with conn.cursor() as cursor:
+            query = """
+                SELECT name , serv FROM persdict WHERE discord_id = %s
+            """
+            val = (id,)
+            await cursor.execute(query,val)
+            result = await cursor.fetchone()
+            if result is not None:
+                name = result[0]
+                realm = result[1]
+            else:
+                name = None
+                realm = None
+    return (name, realm)
+
+
+async def get_embedded_fields(message=None, **kwargs):
+    if not message:
+        channel = kwargs.get('channel')
+        message_id = kwargs.get('id')
+
+        if channel and message_id:
+            message = await channel.fetch_message(message_id)
+
+    # If we could not retrieve a valid message, return
+    if not message:
+        return
+
+    return message.embeds[0] and message.embeds[0].to_dict()['fields']
+
+# endregion
 
 @bot.event
 async def on_error(event, *args, **kwargs):
